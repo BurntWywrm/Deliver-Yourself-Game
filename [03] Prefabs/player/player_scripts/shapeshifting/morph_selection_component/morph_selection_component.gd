b9ffc50f187fc5683b26659404072aaadd33df60
Burnt <burntwyrm.work@gmail.com>
2025-09-08 12:56:28 +1200

    Completed Morph Cylcing
    + Added Cycle CD
    + Added Poof into animation player

--- [03] Prefabs/player/player_scripts/shapeshifting/morph_selection_component/morph_selection_component.gd
+++ [03] Prefabs/player/player_scripts/shapeshifting/morph_selection_component/morph_selection_component.gd
@@ -8,6 +8,9 @@ var morph_selection_change := 1
 const morph_slection_max := 3
 const morph_slection_min := 1
 
+var on_cycle_cd = false
+@onready var cycle_cd_timer: Timer = %Cycle_CD
+
 @export_group("Shapeshifting Morphs")
 @export var envelope_morph: State
 @export var gift_morph: State
@@ -18,11 +21,13 @@ func process_morph_selection(event: InputEvent, current_state: State) -> State:
 	cycle_morph()
 	# Handles selection
 	var next_state: State = select_morph()
-	if next_state != null and next_state != current_state:
-		return next_state
 	if next_state == current_state:
 		print("ERROR: tried to morph into same state")
 		return null
+	if next_state != null and next_state != current_state and on_cycle_cd == false:
+		on_cycle_cd = true
+		cycle_cd_timer.start()
+		return next_state
 	return null
 	
 func cycle_morph()-> void:
@@ -37,13 +42,20 @@ func cycle_morph()-> void:
 			morph_selection_change = morph_slection_max
 
 func select_morph() -> State:
-	if Input.is_action_just_pressed("Select"):
+	if Input.is_action_just_pressed("Select") and on_cycle_cd == false:
 		match morph_selection_change:
 			1: return box_morph
 			2: return gift_morph
 			3: return envelope_morph
 			_: return null
+	# Cycle cooldown error handling
+	elif Input.is_action_just_pressed("Select") and on_cycle_cd == true:
+		print("ERROR: On cooldown")
+		return null
 	return null
 
 func _physics_process(delta: float) -> void:
-	print(morph_selection_change)
+	pass
+
+func _on_cycle_cd_timeout() -> void:
+	on_cycle_cd = false
