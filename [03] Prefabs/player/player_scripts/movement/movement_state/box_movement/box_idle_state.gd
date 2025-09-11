abaef48b508eecc6e0cadd28ea72b892c43dbcfc
Burnt <burntwyrm.work@gmail.com>
2025-09-11 12:45:38 +1200

    Implemented template movement states on box, envelope and gift morphs.

---
+++ [03] Prefabs/player/player_scripts/movement/movement_state/box_movement/box_idle_state.gd
@@ -0,0 +1,18 @@
+extends State
+
+# box_idle_state.gd
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func process_input(event: InputEvent) -> State:
+	return null
+
+func process_frame(delta: float) -> State:
+	return null
+
+func process_physics(delta: float) -> State:
+	return null
